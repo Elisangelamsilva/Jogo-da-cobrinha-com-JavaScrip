c6fe878dea0c4af24fe141d583a8d9cadb8944f9
Elisangela Silva <elisangela00silva@gmail.com>
2020-03-07 23:14:22 -0300

    criar cobrinha

--- script.js
+++ script.js
@@ -8,18 +8,27 @@ snake[0] = {
 }
 
 let direction = "right";
+let food = {
+    x: Math.floor(Math.random() * 15 * 1) * box,
+    y: Math.floor(Math.random() * 15 * 1) * box,
+}
 
 function criarBG() {
-    context.fillstyle = "lightgreen";
+    context.fillStyle = "lightgreen";
     context.fillRect(0, 0, 16 * box, 16 * box);
 
 }
 
 function criarCobrinha() {
     for (i = 0; i < snake.length; i++) {
-        context.fillstyle = "green";
+        context.fillStyle = "green";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
+}
+
+function drawFood() {
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box);
 }
 
 document.addEventListener("keydown", update);
@@ -40,6 +49,7 @@ function iniciarJogo() {
 
     criarBG();
     criarCobrinha();
+    drawFood();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
